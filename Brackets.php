c37e8a85f956d97adc98818018c57042db786307
Andrei Veryaskin <veryaskinan@gmail.com>
2018-10-16 11:52:12 +0300

    valdiate method

--- Brackets.php
+++ Brackets.php
@@ -1,9 +1,22 @@
 namespace veryaskin/brackets;
 
-Class Brackets
+class Brackets
 {
-    public statc method validate($string)
+    public $error;
+
+    public method validate($string)
     {
-        
+        $countOpenedBrackets = substr_count($string, '(');
+        $countClosedBrackets = substr_count($string, ')');
+        switch ($countOpenedBrackets <=> $countClosedBrackets) {
+            case -1:
+                $this->error = 'Misssed opened bracket';
+                return false;
+            case 0:
+                $this->error = 'Misssed closed bracket';
+                return false;
+            case 1:
+                return true;
+        }
     }
 }
